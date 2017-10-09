aba63138b187b9c929fdb5d2ecf94e34c4a3f80f
apurvamandal27 <lalitamandal23@gmail.com>
2017-10-09 14:49:30 +0530

    Initial commit

---
+++ DelFile.java
@@ -0,0 +1,17 @@
+import java.io.File;
+public class DelFile {
+    public static void main(String[] args) {
+		String s="C:\\Users\\apurva\\Desktop";
+		
+        /*File f=new File("C:\\Users\\apurva\\Desktop");
+        File[]arr=f.listFiles();
+        for(File fl:arr){
+            if(fl.getName().endsWith(".txt")){
+                if(fl.length()!=0){
+                    fl.delete();
+                    System.out.println(fl.getName()+"  is deleted");
+                }
+            }
+        }*/
+    }
+}
